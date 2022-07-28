52d0ddf178e6aec37d77330ae5fdeb17c1c56e1b
hironha <rafaelhirokato@gmail.com>
2022-07-28 19:09:11 -0300

    feat: add ws connection

--- frontend/src/pages/Home/Overview/index.tsx
+++ frontend/src/pages/Home/Overview/index.tsx
@@ -1,44 +1,81 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
-  Container,
-  LottieIcon,
-  CardWrapper,
-  CardTitle,
-  CartText,
-  CardsList,
-  ImageLogo,
+	Container,
+	LottieIcon,
+	CardWrapper,
+	CardTitle,
+	CartText,
+	CardsList,
+	ImageLogo,
 } from "./styles";
 
 import Logo from "@assets/images/logo.png";
-
 import humidityIcon from "@assets/lotties/humidity-icon.json";
 import temperatureIcon from "@assets/lotties/temperature-icon.json";
 
+import type { LottieRef } from "lottie-react";
+
+type SensorData = {
+	humidity: number;
+	temperature: number;
+};
+
 export const Overview = () => {
-  // useEffect(() => {
-  //   const ws = new WebSocket('ws://192.0.0.2:80');
-  // }, []);
+	const temperatureRef: LottieRef = useRef(null);
+	const humidityRef: LottieRef = useRef(null);
+	const [sensorData, setSensorData] = useState<SensorData>();
 
-  const formatCelsius = (temperature: number) => {
-    const formatter = new Intl.NumberFormat("pt-br", { notation: "standard" });
-    return `${formatter.format(temperature)} °C`;
-  };
+	const formatCelsius = (temperature: number) => {
+		const formatter = new Intl.NumberFormat("pt-br", {
+			notation: "standard",
+			maximumFractionDigits: 2,
+		});
+		return `${formatter.format(temperature)} °C`;
+	};
 
-  return (
-    <Container>
-      <ImageLogo src={Logo} alt='Logo incubadora' />
-      <CardsList>
-        <CardWrapper>
-          <LottieIcon animationData={humidityIcon} />
-          <CardTitle>Umidade</CardTitle>
-          <CartText>50%</CartText>
-        </CardWrapper>
-        <CardWrapper>
-          <LottieIcon animationData={temperatureIcon} />
-          <CardTitle>Temperatura</CardTitle>
-          <CartText>{formatCelsius(35.7)}</CartText>
-        </CardWrapper>
-      </CardsList>
-    </Container>
-  );
+	const formatHumidity = (humidity: number) => {
+		const formatter = new Intl.NumberFormat("pt-br", {
+			notation: "standard",
+			maximumFractionDigits: 2,
+		});
+		return `${formatter.format(humidity)}%`;
+	};
+
+	useEffect(() => {
+		const url = "ws://192.168.0.2:80/incubator/listen";
+
+		const ws = new WebSocket(url);
+
+		ws.onmessage = async event => {
+			console.log(event.data);
+			setSensorData(JSON.parse(event.data));
+			console.log(temperatureRef);
+			temperatureRef.current?.goToAndPlay(0);
+			humidityRef.current?.goToAndPlay(0);
+		};
+
+		return () => {
+			ws.close();
+		};
+	}, []);
+
+	return (
+		<Container>
+			<ImageLogo src={Logo} alt="Logo incubadora" />
+			<CardsList>
+				<CardWrapper>
+					<LottieIcon animationData={humidityIcon} loop={false} lottieRef={humidityRef} />
+					<CardTitle>Umidade</CardTitle>
+					<CartText>{sensorData ? formatHumidity(sensorData.humidity) : "Não registrado"}</CartText>
+				</CardWrapper>
+				<CardWrapper>
+					<LottieIcon animationData={temperatureIcon} lottieRef={temperatureRef} loop={false} />
+					<CardTitle>Temperatura</CardTitle>
+					<CartText>
+						{sensorData ? formatCelsius(sensorData.temperature) : "Não Registrado"}
+					</CartText>
+				</CardWrapper>
+			</CardsList>
+		</Container>
+	);
 };
